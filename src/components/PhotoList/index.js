9691eacff2bf55ab58545e20730e454249eedf23
Kris Renaldi <krenaldi@comcast.net>
2020-10-02 12:40:24 -0400

    Added tests to check if Modal component renders and gets fired on event click

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -107,12 +107,12 @@ export default function PhotoList({category}) {
 
     const toggleModal = (image, i) => {
         setCurrentPhoto({...image, index: i});
-        setIsModalOpen(true);
+        setIsModalOpen(!isModalOpen);
     }
     
     return (
         <div>
-            {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+            {isModalOpen && <Modal currentPhoto={currentPhoto} onClose={toggleModal} />}
             <div className="flex-row">
                 {currentPhotos.map((image, i) => (
                     <img src={require(`../../assets/small/${category}/${i}.jpg`)} alt={image.name}  className="img-thumbnail mx-1" onClick={() => toggleModal(image, i)} key={image.name} />
